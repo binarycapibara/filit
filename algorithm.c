fde3190e025dda20c425f57aa1c28bf0ea7a7f45
Fernande Jenae <fjenae@oa-h5.21-school.ru>
2019-07-07 21:24:54 +0300

    0

--- algorithm.c
+++ algorithm.c
@@ -6,7 +6,7 @@
 /*   By: fjenae <fjenae@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 19:43:21 by fjenae            #+#    #+#             */
-/*   Updated: 2019/07/07 21:22:39 by fjenae           ###   ########.fr       */
+/*   Updated: 2019/07/07 21:24:41 by fjenae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 static		ft_box_calc (int	**N, char		***box)
 {
-	N = 
-
-
-
+	int		res;
+	
+	res = *N;
+	res = ft_sqrt(res * 4);
+	*N = res;
+	return ;
 }
 
 
@@ -34,13 +36,5 @@ ft_core_algo(int	*N, char	**sh_coord)
 	char		**box[0][0];
 
 	ft_box_calc(&N, &box);
-
-
-
-
-
-
-
-
-
+	return ;
 }
