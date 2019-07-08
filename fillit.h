f6bb23f453e15894899809dfedc471be27f9a4e5
Drizzle Rafe <drafe@oa-j5.21-school.ru>
2019-07-08 17:59:51 +0300

    placing shapes in wrong place

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: drafe <drafe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 15:07:55 by drafe             #+#    #+#             */
-/*   Updated: 2019/07/07 19:13:46 by drafe            ###   ########.fr       */
+/*   Updated: 2019/07/08 17:33:30 by drafe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ typedef struct		s_tetris
 	int			y[4];
 }					t_tetris;
 
-int					ft_box(int sh_nb, t_tetris *all_sh);
+int			ft_box_insert_shape(int m, char min_box[m][m], t_tetris *all_sh);
+int			ft_box_size(int n);
+int			ft_box_create(int m, t_tetris *all_sh);
+int			ft_box(int sh_nb, t_tetris *all_sh);
 
 #endif
--- ft_box.c
+++ ft_box.c
@@ -6,17 +6,47 @@
 /*   By: drafe <drafe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 18:53:53 by drafe             #+#    #+#             */
-/*   Updated: 2019/07/08 15:17:29 by drafe            ###   ########.fr       */
+/*   Updated: 2019/07/08 17:58:35 by drafe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
 /* **************************************************************************
+**	----int ft_box_insert_shape(int m, char min_box[m][m])----
+**	Function place shape in min_box
+** **************************************************************************
+*/
+
+int			ft_box_insert_shape(int m, char min_box[m][m], t_tetris *all_sh)
+{
+	char	a;
+	int		i;
+	int		j;
+
+	i = 0;
+	a = 'A';
+	while (i < m)
+	{
+		j = 0;
+		while (j < m)
+		{
+			printf("i=%d j=%d all_sh=%d\n", i, j, all_sh[i].x[j]);
+				min_box[i + all_sh[i].x[j]][i + all_sh[i].y[j]] = a;
+			j++;
+		}
+		a++;
+		i++;
+	}
+	//ft_box_create(m++, all_sh);
+	return (0);
+}
+
+/* **************************************************************************
 **	----int	ft_box_size(int n)----
-**	Function count size of minimum possible box. N= 26 box(11 * 11)
+**	Function count size of minimum possible box. N = 26 >>> m = 11
 ** -------------------------------------------------------------------------
-**	----Test print ft_box_size (n [0..26])----
+**	----Test print for ft_box_size (n [0..26])----
 **	nbr = -1; 
 **	while (nbr++ < 26)
 **	{
@@ -41,16 +71,59 @@ int			ft_box_size(int n)
 	return (m - 1);
 }
 
-int			ft_box(int sh_nb, t_tetris *all_sh)
+/* **************************************************************************
+**	----int	ft_box_create(int m, t_tetris *all_sh)----
+**	Function create array min_box for handle our shapes.
+**	Array initialized with '.'
+** **************************************************************************
+*/
+
+int	ft_box_create(int m, t_tetris *all_sh)
 {
-	char	min_box[ft_box_size(sh_nb)][ft_box_size(sh_nb)];
+	char	min_box[m][m];
 	int		i;
-	int		nbr;
+	int		j;
 
 	i =	0;
-	nbr = all_sh[0].x[0]; 
-	nbr = ft_box_size(sh_nb);
+	while (i < m)
+	{
+		j = 0;
+		while (j < m)
+		{
+			min_box[i][j] = '.';
+			j++;
+		}
+		i++;
+	}
+	ft_box_insert_shape(m, min_box, all_sh);
+	i =	0;
+	while (i < m)
+	{
+		j = 0;
+		while (j < m)
+		{
+			printf ("%c", min_box[i][j]);
+			j++;
+		}
+		printf ("\n");
+		i++;
+	}
+	return (0);
+}
+
+/* **************************************************************************
+**	----int	ft_box(int n)----
+**	Function for send m to 
+** **************************************************************************
+*/
+
+int			ft_box(int sh_nb, t_tetris *all_sh)
+{
+	int		m;
+
+	m = all_sh[0].x[0]; 
+	m = ft_box_size(sh_nb);
+	ft_box_create(m, all_sh);
 	//printf("%d * %d", nbr, nbr);
-	min_box[0][0] = '\0';
 	return (0);
 }
